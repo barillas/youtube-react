17158f2bb94873c09c47b12eda0aea25342f4751
Oscar Barillas <oscarbarillas93@gmail.com>
2016-08-27 16:33:45 -0700

    Working project wtih CSS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 //Install React
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 
@@ -9,29 +10,37 @@ import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBQ-W90YAsV66Q4STWg4kHHLti_nPTvvmE';
 
-
-
-
 class App extends Component {
   constructor(props){
     super(props);
 
-    this.state = {videos: []};
+    this.state = {
 
-      YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({videos});
+      videos: [],
+      selectedVideo: null
+    };
 
+    this.videoSearch('surfboards')
+  }
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+    this.setState({
+      videos: videos,
+      selectedVideo: videos[0]
       });
-
-
+    });
   }
 
   render() {
+
+  const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 300);
   return (
     <div>
-    <SearchBar />
-    <VideoDetail video = {this.state.videos[0]} />
-    <VideoList videos = {this.state.videos} />
+    <SearchBar onSearchTermChange = {videoSearch } />
+    <VideoDetail video = {this.state.selectedVideo} />
+    <VideoList
+    onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+    videos = {this.state.videos} />
     </div>
   )
 }
