34c515afb21e9b55917446c4f1458eef83c129b0
Oscar Barillas <oscarbarillas93@gmail.com>
2016-08-27 11:16:28 -0700

    Styled video list

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
 //Install React
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-import SearchBar from './components/search_bar'
 
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list.js';
+import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyBQ-W90YAsV66Q4STWg4kHHLti_nPTvvmE';
 
-//Create a new component. //This component should produce some HTML
 
-const App = () => {
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {videos: []};
+
+      YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({videos});
+
+      });
+
+
+  }
+
+  render() {
   return (
+    <div>
     <SearchBar />
+    <VideoList videos = {this.state.videos} />
+    </div>
   )
 }
-
-//Take this component's generated HTMl and put it on the page // In the DOM
+}
 
 ReactDom.render(<App />, document.querySelector('.container'));
